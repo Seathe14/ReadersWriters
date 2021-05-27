1e770262148dc67151433181d774f69d90b2b2af
Imynn <seathe14@gmail.com>
2021-05-27 21:52:00 +0300

    Done

--- TaskReadersWriters/TaskReadersWriters.cpp
+++ TaskReadersWriters/TaskReadersWriters.cpp
@@ -2,26 +2,35 @@
 
 #include <iostream>
 #include <windows.h>
-#include <thread>
 #include <tchar.h>
-#define NUMREADERS 3
-#define NUMWRITERS 10
-#define NUMREPEAT 5
 
-#define TIMEDELAY 300
+#define NUMREADERS 5
+#define NUMWRITERS 20
+#define NUMREPEAT 5
+#define TIMEDELAY 50
 #define BUFSIZE 128
+// global variables
 LPCTSTR lpFileName = _T("SharedFile.txt");
-std::thread readers[NUMREADERS];
-std::thread writers[NUMWRITERS];
-HANDLE hEvent;
-HANDLE hWriterEvent;
+bool bWritersActive = false;
+
+// global Handles
+HANDLE hSignalEvent;
 HANDLE hWriterMutex;
 HANDLE hReaderMutex;
-CRITICAL_SECTION CriticalSection;
-bool bWritersActive = true;
-void writeToFile(int id)
+CRITICAL_SECTION CriticalSection;	
+
+
+void CloseHandleArr(HANDLE* arr, int size)
 {
-	WaitForSingleObject(hWriterEvent, INFINITE);
+	for (int i = 0; i < size; i++)
+	{
+		CloseHandle(arr[i]);
+	}
+}
+DWORD WINAPI writeToFile(void* ptrID)
+{
+	int id = (int)ptrID;
+	WaitForSingleObject(hSignalEvent, INFINITE);
 	WaitForSingleObject(hWriterMutex, INFINITE);
     DWORD cbWritten;
     HANDLE hFile = CreateFile(lpFileName, FILE_APPEND_DATA , FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -32,36 +41,36 @@ void writeToFile(int id)
         if (hFile == INVALID_HANDLE_VALUE)
         {
             std::cout << "File creating failed. The code of the error: " << GetLastError() << std::endl;
-            return;
+            return 1;
         }
     }
 	char buf[BUFSIZE] = "";
 	for (int i = 0; i < BUFSIZE; i++)
 		buf[i] = '\0';
     buf[BUFSIZE - 1] = '\n';
-    char toWrite[32];
+	char toWrite[BUFSIZE];
     for (int i = 0; i < NUMREPEAT;i++)
     {
 		std::cout << "Writer #" << id << " writes " << i << std::endl;
-		sprintf(toWrite, "This is a %d writer %d", id,i);
+		sprintf(toWrite, "This is a %d writer, %d iteration", id,i);
 		strcpy(buf, toWrite);
         WriteFile(hFile, buf, sizeof(buf), &cbWritten, NULL);
         Sleep(TIMEDELAY);
     }
 	ReleaseMutex(hWriterMutex);
-	SetEvent(hWriterEvent);
-
+	SetEvent(hSignalEvent);
+	CloseHandle(hFile);
+	return 0;
 }
-void readFromFile(int id)
+DWORD WINAPI readFromFile(void* ptrID)
 {
-    
+	int id = (int)ptrID;
 	DWORD cbRead;
-	std::thread::id t_id = readers[id].get_id();
 	HANDLE hFile = CreateFile(lpFileName, GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
 		std::cout << "File does not exist\n";
-		return;
+		return 1;
 	}
 	char buf[BUFSIZE];
 	static int readers = 0;
@@ -71,7 +80,7 @@ void readFromFile(int id)
 		readers++;
 		if (readers == 1)
 		{
-			WaitForSingleObject(hWriterEvent, INFINITE);
+			WaitForSingleObject(hSignalEvent, INFINITE);
 		}
 		ReleaseMutex(hReaderMutex);
 		while (ReadFile(hFile, buf, sizeof(buf), &cbRead, NULL))
@@ -86,38 +95,61 @@ void readFromFile(int id)
 		readers--;
 		if (readers == 0)
 		{
-			SetEvent(hWriterEvent);
+			SetEvent(hSignalEvent);
 		}
 		ReleaseMutex(hReaderMutex);
 	}
+	CloseHandle(hFile);
+
+	return 0;
 }
-int main()  
-{	
-    hEvent = CreateEvent(NULL, FALSE, FALSE, _T("isDoneEvent"));
-	hWriterEvent = CreateEvent(NULL, FALSE, TRUE, _T("isWriterDoneEvent"));
-    hWriterMutex = CreateMutex(NULL, FALSE, _T("WriterMutex"));
-	hReaderMutex = CreateMutex(NULL, FALSE, _T("ReaderMutex"));
-
-    InitializeCriticalSection(&CriticalSection);
-	for (int i = 0; i < NUMREADERS; i++)
-	{
-		readers[i] = std::thread(readFromFile, i);
-	}
-
+DWORD WINAPI runWriters(void *)
+{
+	HANDLE writers[NUMWRITERS];
+	bWritersActive = true;
 	for (int i = 0; i < NUMWRITERS; i++)
 	{
-		writers[i] = std::thread(writeToFile, i);
+		writers[i] = CreateThread(NULL, 0, writeToFile, (void*)i, NULL, 0);;
 	}
-
-    for (int i = 0; i < NUMWRITERS; i++)
-    {
-		writers[i].join();
-    }
+	WaitForMultipleObjects(NUMWRITERS, writers, true, INFINITE);
 	bWritersActive = false;
+	CloseHandleArr(writers, NUMWRITERS);
+	return 0;
+}
+DWORD WINAPI runReaders(void*)
+{
+	HANDLE readers[NUMREADERS];
+	InitializeCriticalSection(&CriticalSection);
+	while (!bWritersActive) // to prevent the situation when readers start reading an empty file.
+		Sleep(1);
 	for (int i = 0; i < NUMREADERS; i++)
 	{
-		readers[i].join();
+		readers[i] = CreateThread(NULL, 0, readFromFile, (void*)i, NULL, 0);
 	}
-    DeleteCriticalSection(&CriticalSection);
+	WaitForMultipleObjects(NUMREADERS, readers, true, INFINITE);
+	DeleteCriticalSection(&CriticalSection);
+	CloseHandleArr(readers, NUMREADERS);
+	return 0;
 }
 
+int main()  
+{
+	HANDLE hFile = CreateFile(lpFileName, GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	hSignalEvent = CreateEvent(NULL, FALSE, TRUE, _T("SignalEvent"));
+    hWriterMutex = CreateMutex(NULL, FALSE, _T("WriterMutex"));
+	hReaderMutex = CreateMutex(NULL, FALSE, _T("ReaderMutex"));
+	HANDLE RWThreads[] =
+	{
+		CreateThread(NULL, 0, runReaders, nullptr, 0, 0),
+		CreateThread(NULL, 0, runWriters, nullptr, 0, 0)
+	};	
+	WaitForMultipleObjects(2, RWThreads, true, INFINITE);
+	CloseHandleArr(RWThreads, 2);
+	CloseHandle(hFile);
+	if(hReaderMutex != NULL)
+		CloseHandle(hReaderMutex);
+	if(hSignalEvent != NULL)
+		CloseHandle(hSignalEvent);
+	if(hWriterMutex != NULL)
+		CloseHandle(hWriterMutex);
+}
