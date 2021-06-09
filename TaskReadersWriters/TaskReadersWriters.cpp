fe5dfb55d5949d98111b95ccca53b3be2d1557ac
Imynn <seathe14@gmail.com>
2021-06-10 00:29:45 +0300

    Before changes on size of array

--- TaskReadersWriters/TaskReadersWriters.cpp
+++ TaskReadersWriters/TaskReadersWriters.cpp
@@ -19,7 +19,9 @@ HANDLE hWriterMutex;
 HANDLE hReaderMutex;
 CRITICAL_SECTION CriticalSection;	
 
-
+char SharedMemoryThing[NUMWRITERS * NUMREPEAT];
+int SharedMemory[NUMWRITERS * NUMREPEAT];
+std::string SharedMemoryExample;
 void CloseHandleArr(HANDLE* arr, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -27,11 +29,14 @@ void CloseHandleArr(HANDLE* arr, int size)
 		CloseHandle(arr[i]);
 	}
 }
+int cellNumber = 0;
 DWORD WINAPI writeToFile(void* ptrID)
 {
 	int id = (int)ptrID;
 	WaitForSingleObject(hSignalEvent, INFINITE);
 	WaitForSingleObject(hWriterMutex, INFINITE);
+	bWritersActive = true;
+
     DWORD cbWritten;
     HANDLE hFile = CreateFile(lpFileName, FILE_APPEND_DATA , FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE)
@@ -54,12 +59,18 @@ DWORD WINAPI writeToFile(void* ptrID)
 		std::cout << "Writer #" << id << " writes " << i << std::endl;
 		sprintf(toWrite, "This is a %d writer, %d iteration", id,i);
 		strcpy(buf, toWrite);
-        WriteFile(hFile, buf, sizeof(buf), &cbWritten, NULL);
+		SharedMemoryThing[cellNumber] = id+48;
+		//SharedMemory[cellNumber] = i;
+        //WriteFile(hFile, buf, sizeof(buf), &cbWritten, NULL);
+		//SharedMemoryExample.append();
+		cellNumber++;
+
         Sleep(TIMEDELAY);
     }
 	ReleaseMutex(hWriterMutex);
 	SetEvent(hSignalEvent);
 	CloseHandle(hFile);
+	bWritersActive = false;
 	return 0;
 }
 DWORD WINAPI readFromFile(void* ptrID)
@@ -74,8 +85,11 @@ DWORD WINAPI readFromFile(void* ptrID)
 	}
 	char buf[BUFSIZE];
 	static int readers = 0;
-	while(bWritersActive)
+	int readCell = 0;
+	while(true)
 	{
+		while (bWritersActive)
+		{
 		WaitForSingleObject(hReaderMutex, INFINITE);
 		readers++;
 		if (readers == 1)
@@ -83,21 +97,59 @@ DWORD WINAPI readFromFile(void* ptrID)
 			WaitForSingleObject(hSignalEvent, INFINITE);
 		}
 		ReleaseMutex(hReaderMutex);
-		while (ReadFile(hFile, buf, sizeof(buf), &cbRead, NULL))
-		{
-			if (cbRead == 0)
+		//int* i = SharedMemory;
+		//while(*i != 0)
+		//while(cellNumber <= NUMREPEAT*NUMWRITERS)
+		//if(!bWritersActive)
+		
+			for (;readCell < cellNumber; readCell++)
+			{
+				EnterCriticalSection(&CriticalSection);
+				std::cout << SharedMemoryThing[readCell] << " record read by " << id << " reader thread" << std::endl;
+				//std::cout << SharedMemory[readCell] << " record read by " << id << " reader thread" << std::endl;
+				LeaveCriticalSection(&CriticalSection);
+			}
+			//while(i!=NUMREPEAT*NUMWRITERS)
+			//while (SharedMemory[i] != -1)
+			//{
+			//	EnterCriticalSection(&CriticalSection);
+			//	std::cout << SharedMemory[i] << " record read by " << id << " reader thread" << std::endl;
+			//	LeaveCriticalSection(&CriticalSection);
+			//	i++;
+			//}
+			//for (int i = 0; i < NUMREPEAT; i++)
+			//{
+			//
+			//	EnterCriticalSection(&CriticalSection);
+			//	std::cout << SharedMemory[i] << " record read by " << id << " reader thread" << std::endl;
+			//	LeaveCriticalSection(&CriticalSection);
+			//}
+			//while (ReadFile(hFile, buf, sizeof(buf), &cbRead, NULL))
+			//{
+			//	if (cbRead == 0)
+			//		break;
+			//	EnterCriticalSection(&CriticalSection);
+			//	std::cout << buf << " read by " << id << " reader thread" << std::endl;
+			//	LeaveCriticalSection(&CriticalSection);
+			//}
+			WaitForSingleObject(hReaderMutex, INFINITE);
+			readers--;
+			if (readers == 0)
+			{
+				SetEvent(hSignalEvent);		
+				ReleaseMutex(hReaderMutex);
 				break;
-			EnterCriticalSection(&CriticalSection);
-			std::cout << buf << " read by " << id << " reader thread" << std::endl;
-			LeaveCriticalSection(&CriticalSection);
+			}
+
+			ReleaseMutex(hReaderMutex);
+			//if (readers == 0)
+				//break;
 		}
-		WaitForSingleObject(hReaderMutex, INFINITE);
-		readers--;
-		if (readers == 0)
-		{
-			SetEvent(hSignalEvent);
-		}
-		ReleaseMutex(hReaderMutex);
+		if (readCell == NUMREPEAT*NUMWRITERS)
+			break;
+		//if (cellNumber == NUMREPEAT * NUMWRITERS && bWritersActive == false)
+		//	break;
+
 	}
 	CloseHandle(hFile);
 
@@ -135,9 +187,10 @@ DWORD WINAPI runReaders(void*)
 int main()  
 {
 	HANDLE hFile = CreateFile(lpFileName, GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	hSignalEvent = CreateEvent(NULL, FALSE, TRUE, _T("SignalEvent"));
-    hWriterMutex = CreateMutex(NULL, FALSE, _T("WriterMutex"));
-	hReaderMutex = CreateMutex(NULL, FALSE, _T("ReaderMutex"));
+	std::fill(SharedMemory, SharedMemory + sizeof(SharedMemory) / sizeof(int), -1);
+	//hSignalEvent = CreateEvent(NULL, FALSE, TRUE, _T("SignalEvent"));
+    //hWriterMutex = CreateMutex(NULL, FALSE, _T("WriterMutex"));
+	//hReaderMutex = CreateMutex(NULL, FALSE, _T("ReaderMutex"));
 	HANDLE RWThreads[] =
 	{
 		CreateThread(NULL, 0, runReaders, nullptr, 0, 0),
